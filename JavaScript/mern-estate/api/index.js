48e2e0eedecc02a195ac3ab929b8e6f50fd0b554
Bhav20002 <bhavagarwal226@gmail.com>
2024-06-25 12:03:39 +0530

    Create a test api route

--- JavaScript/mern-estate/api/index.js
+++ JavaScript/mern-estate/api/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
 
 const app = express();
 
-app.listen(7000 , () =>{
+app.listen(7000, () => {
     console.log("Server is running on port 7000");
 });
+
+app.use('/api/user' ,userRouter)
