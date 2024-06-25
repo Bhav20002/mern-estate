2cf4ddc00d0a360f98b3044bbbf5b02ae418fc10
Bhav20002 <bhavagarwal226@gmail.com>
2024-06-25 14:52:40 +0530

    create a middleware and a function to handle possible errors

--- JavaScript/mern-estate/api/index.js
+++ JavaScript/mern-estate/api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import  authRouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -21,5 +21,15 @@ app.listen(7000, () => {
     console.log("Server is running on port 7000");
 });
 
-app.use('/api/user' ,userRouter);
-app.use('/api/auth' ,authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        sucess: false,
+        statusCode,
+        message,
+    });
+});
