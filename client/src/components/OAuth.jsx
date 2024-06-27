7a72a9ae8410f109c438436ee73dbe74c7e0cedf
Bhav20002 <bhavagarwal226@gmail.com>
2024-06-27 20:50:16 +0530

    Added profile picture functionality and also fixed some major bugs

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 export default function OAuth() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const handleGoogleclick = async () => {
+    const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app);
@@ -17,35 +17,28 @@ export default function OAuth() {
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     name: result.user.displayName,
                     email: result.user.email,
-                    photo: result.user.photoURL
+                    photo: result.user.photoURL,
                 }),
             });
-
-            // Check if the response is empty
-            if (res.ok) {
-                const text = await res.text();
-                if (text) {
-                    const data = JSON.parse(text);
-                    dispatch(signInSuccess(data));
-                    navigate('/');
-                } else {
-                    console.log('Empty response from server');
-                }
-            } else {
-                console.log('Server returned an error', res.statusText);
-            }
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+            navigate('/');
         } catch (error) {
-            console.log('Could not sign in with Google', error);
+            console.log('could not sign in with google', error);
         }
     };
     return (
-        <button onClick={handleGoogleclick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
-            Continue with Google
+        <button
+            onClick={handleGoogleClick}
+            type='button'
+            className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+        >
+            Continue with google
         </button>
     );
 }
